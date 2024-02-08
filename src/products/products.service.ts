a21c7f8910f9dc01faf1edef453af9b777de5b99
Samantha Buonato <sb2024@icloud.com>
2024-02-08 07:57:28 -0500

    9 mins left)

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,6 +1,38 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
+
+import { Product } from './product.model'
 
 @Injectable()
 export class ProductsService {
-    
+    products: Product[] = [];
+
+    insertProduct(title: string, desc: string, price: number) {
+        const prodId = Math.random().toString();
+        const newProduct = new Product(new Date().toString(), title, desc, price);
+        this.products.push(newProduct);
+        return prodId;
+    }
+
+    getProducts() {
+        return [...this.products];
+    }
+
+    getSingleProduct(productId: string) {
+        const product = this.findProduct(productId)[0];
+        return {...product};
+    }
+
+    updateProduct(productId: string, title: string, desc: string, price: number) {
+        const product = this.findProduct(productId);
+
+    }
+
+    private findProduct(id: string): [Product, number] {
+        const productIndex = this.products.findIndex(prod => prod.id === id);
+        const product = this.products[productIndex];
+        if (!product) {
+            throw new NotFoundException('Could not find product.');
+        }
+        return [product, productIndex];
+    }
 }
